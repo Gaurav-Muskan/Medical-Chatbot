b109074790aa7aaeb0f8bb89d598a18247c0e87b
Gaurav Muskan <=>
2026-06-13 20:44:04 +0530

    Added chat history persistence using Streamlit session state

--- medicalChatbot.py
+++ medicalChatbot.py
@@ -3,13 +3,22 @@ import streamlit as st
 def main():
     st.title("Ask Chatbot!")
 
+    if 'messages' not in st.session_state:
+        st.session_state.messages = []
+    
+    for message in st.session_state.messages:
+        st.chat_message(message['role']).markdown(message['content'])
+
     prompt = st.chat_input("Pass your prompt here")
 
     if prompt:
         st.chat_message('user').markdown(prompt)
+        st.session_state.messages.append({'role':'user', 'content':prompt})
 
         response="Hi, I am MediBot!"
         st.chat_message('assistant').markdown(response)
+        st.session_state.messages.append({'role': 'assistant', 'content': response})
+
 
 if( __name__) == "__main__":
     main()
